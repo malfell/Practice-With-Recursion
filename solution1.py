c15be7d30ca90ca726e8b18e9983623082a1ef7b
Mal F <malfellows13@gmail.com>
2022-09-18 11:45:36 -0500

    solution 1 done

--- solution1.py
+++ solution1.py
@@ -1,1 +1,14 @@
 # Write code for algorithm 1 below
+# Write a program that takes a positive number as an argument 
+# and prints the numbers from n to zero.
+
+def to_zero(num):
+    # Base
+    if num == 0:
+        return
+    # Recursive
+    else:
+        print(num)
+        to_zero(num - 1)
+
+to_zero(6)
